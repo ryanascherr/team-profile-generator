a9b6207ff28255868fb4dcf982737c519d1d82b6
ryanascherr <ryanascherr@gmail.com>
2021-04-25 21:48:46 -0400

    Added tests for Intern class.

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -7,26 +7,26 @@ test("Can instantiate Engineer instance", () => {
 
 test("Returns name of Engineer", () => {
     const ryan = new Engineer("Ryan", 1, "ryanascherr@gmail.com", "ryanascherr");
-    expect(ryan.getName).toBe("Ryan")
+    expect(ryan.getName()).toBe("Ryan")
 })
 
 test("Returns ID of Engineer", () => {
     const ryan = new Engineer("Ryan", 1, "ryanascherr@gmail.com", "ryanascherr");
-    expect(ryan.getId).toBe(1)
+    expect(ryan.getId()).toBe(1)
 })
 
 test("Returns email of Engineer", () => {
     const ryan = new Engineer("Ryan", 1, "ryanascherr@gmail.com", "ryanascherr");
-    expect(ryan.getEmail).toBe("ryanascherr@gmail.com")
+    expect(ryan.getEmail()).toBe("ryanascherr@gmail.com")
 })
 
 test("Returns GitHub username of Engineer", () => {
     const ryan = new Engineer("Ryan", 1, "ryanascherr@gmail.com", "ryanascherr");
-    expect(ryan.getGithub).toBe("ryanascherr")
+    expect(ryan.getGithub()).toBe("ryanascherr")
 })
 
-test("Returns role Engineer", () => {
+test("Returns role of Engineer", () => {
     const ryan = new Engineer("Ryan", 1, "ryanascherr@gmail.com", "ryanascherr");
-    expect(ryan.getRole).toBe("Engineer")
+    expect(ryan.getRole()).toBe("Engineer")
 })
 
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,6 +1,32 @@
 const Intern = require("../lib/Intern");
 
 test("Can instantiate Employee instance", () => {
-    const e = new Intern();
-    expect(typeof(e)).toBe("object")
+    const ryan = new Intern("Ryan", 1, "ryanascherr@gmail.com", "Loyola");
+    expect(typeof(ryan)).toBe("object")
 })
+
+test("Returns name of Intern", () => {
+    const ryan = new Intern("Ryan", 1, "ryanascherr@gmail.com", "Loyola");
+    expect(ryan.getName()).toBe("Ryan")
+})
+
+test("Returns ID of Intern", () => {
+    const ryan = new Intern("Ryan", 1, "ryanascherr@gmail.com", "Loyola");
+    expect(ryan.getId()).toBe(1)
+})
+
+test("Returns email of Intern", () => {
+    const ryan = new Intern("Ryan", 1, "ryanascherr@gmail.com", "Loyola");
+    expect(ryan.getEmail()).toBe("ryanascherr@gmail.com")
+})
+
+test("Returns school of Intern", () => {
+    const ryan = new Intern("Ryan", 1, "ryanascherr@gmail.com", "Loyola");
+    expect(ryan.getSchool()).toBe("Loyola")
+})
+
+test("Returns role of Intern", () => {
+    const ryan = new Intern("Ryan", 1, "ryanascherr@gmail.com", "Loyola");
+    expect(ryan.getRole()).toBe("Intern")
+})
+
